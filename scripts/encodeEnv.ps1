e437125c5691818859e58c803bf23387d586992a
Daniel Salazar <dsnile.salazr@gmail.com>
2025-10-05 17:43:34 -0600

    script to encoded and add secrets from envfile

---
+++ scripts/encodeEnv.ps1
@@ -0,0 +1,12 @@
+$content = Get-Content ..\.env  
+
+foreach($line in $content){
+    $split = $line.Split("=") 
+    $key = $split[0]
+    $len = $split.Length
+    $value = $split[1.. ($len - 1)] -join ""
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($value)
+    $encoded = [System.Convert]::ToBase64String($bytes)
+    Add-Content ..\secrets.yml -Value "$key : $encoded"
+}
+
