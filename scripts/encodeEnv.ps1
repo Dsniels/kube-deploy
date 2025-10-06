32d0e35a749efeb5f9adc0f8742eddc5069f7ba3
Daniel Salazar <dsnile.salazr@gmail.com>
2025-10-05 18:21:14 -0600

    fix script

--- scripts/encodeEnv.ps1
+++ scripts/encodeEnv.ps1
@@ -1,10 +1,14 @@
 $content = Get-Content ..\.env  
 
-foreach($line in $content){
+foreach ($line in $content) {
     $split = $line.Split("=") 
     $key = $split[0]
+    $split = $split | ForEach-Object {
+        $_.Trim()
+    }
     $len = $split.Length
-    $value = $split[1.. ($len - 1)] -join ""
+    $value = $split[1.. ($len - 1)] -join "="
+    Write-Host $value
     $bytes = [System.Text.Encoding]::UTF8.GetBytes($value)
     $encoded = [System.Convert]::ToBase64String($bytes)
     Add-Content ..\secrets.yml -Value "$key : $encoded"
